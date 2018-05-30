866fd74275c7ba2c0a04c956f49b06f49e5fc612
402490014 <39294965+402490014@users.noreply.github.com>
2018-05-31 00:30:56 +0800

    Add files via upload
    Demo: 將database裡的資料進行讀取後顯示於eclipse的console

---
+++ ReadCardDemo.java
@@ -0,0 +1,45 @@
+package testLoadingCard;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ReadCardDemo {
+
+	public static void main(String[] args) {
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+		} catch (Exception ex) {
+			// handle the error
+		}
+
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/databased?"
+					+ "user=root&password=102280133!&serverTimezone=UTC&useSSL=false");
+
+			Statement stmt = conn.createStatement();
+			
+			//ResultSet rs = stmt.executeQuery("select a.num, subject, verb, object, action, cardname, card" + " from action a, cards c" + " where c.cardnum = a.cardname");
+			
+			ResultSet rs = stmt.executeQuery("select c.cardnum, card" + " from cards c");
+			
+			while (rs.next()) {
+				System.out.println(
+						//rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getString(4) + "\t" + rs.getString(5) + "\t" + rs.getString(6) + "\t" + rs.getString(7));
+						rs.getInt(1) + "\t" + rs.getString(2) + "\t");
+			}
+
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+		}
+
+	}
+}
