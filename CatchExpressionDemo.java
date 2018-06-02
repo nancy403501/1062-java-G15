98af2d07f79cba6cd68fc94a27bfb0dbb5c920be
402490014 <39294965+402490014@users.noreply.github.com>
2018-06-02 10:04:04 +0800

    Add files via upload
    Demo: 從資料庫中抓取描述句

---
+++ CatchExpressionDemo.java
@@ -0,0 +1,74 @@
+package test2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CatchExpressionDemo {
+
+	private String expression =  null ;
+	private int exCount = 0;
+
+	private String subject = null;
+	private String verb = null;
+	private String object = null;
+
+	public void getSubOrOb(String card) {
+		if (subject != null) {
+			object = card;
+			System.out.println("object: " + object);
+		} else {
+			subject = card;
+			System.out.println("subject: " + subject);
+		}
+	}
+
+	public void getVerb(String card) {
+		verb = card;
+		System.out.println("verb: " + verb);
+	}
+
+	public String getExpression() {
+		if (subject != null && verb != null && object != null) {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			} catch (Exception ex) {
+				// handle the error
+			}
+
+			Connection conn = null;
+			try {
+				conn = DriverManager.getConnection("jdbc:mysql://localhost/databased?"
+						+ "user=root&password=0000&serverTimezone=UTC&useSSL=false");
+
+				Statement stmt = conn.createStatement();
+
+				ResultSet rs = stmt.executeQuery("select a.num, subject, verb, object, action, tocardnum, card"
+						+ " from action a, cards c" + " where c.cardnum = a.tocardnum" + " and subject='" + subject
+						+ "'" + " and verb='" + verb + "'" + " and object='" + object + "'");
+
+				while (rs.next()) {
+					expression = rs.getString(5);
+					System.out.println("expression: " + expression);
+				}
+
+			} catch (SQLException ex) {
+				// handle any errors
+				System.out.println("SQLException: " + ex.getMessage());
+				System.out.println("SQLState: " + ex.getSQLState());
+				System.out.println("VendorError: " + ex.getErrorCode());
+			}
+
+			subject = null;
+			verb = null;
+			object = null;
+
+			return expression;
+		} else {
+			return expression;
+		}
+	}
+
+}
